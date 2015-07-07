9a5b8f0b84f97836ca56e4b22d3ea78230b7ddd8
sergiocasquejo <serg.casquejo@yahoo.com>
2015-07-07 23:43:17 +0800

    update

---
+++ wp-content/themes/atu/content-vendors.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * We start by doing a query to retrieve all users
+ * We need a total user count so that we can calculate how many pages there are
+ */
+
+$term = get_query_var( 'term' );
+
+$search_args = array();
+
+$count_args  = array(
+    'role'      => 'Vendor',
+    'fields'    => 'all_with_meta',
+    'number'    => 999999
+);
+
+if ( isset( $_GET['keyword'] ) && isset( $_GET['profession'] ) && ! empty( $_GET['profession'] ) ) {
+    $search_args['search'] = '*'.esc_attr( $_GET['keyword'] ).'*';
+
+
+    $search_args['meta_query'] = array(
+        'relation'  => 'AND',
+        array(
+            'key'       => 'profession',
+            'value'     => esc_attr( $_GET['profession'] ),
+            'compare'   => '='
+        ),
+        array(
+            'key'     => 'company_name',
+            'value'   => $_GET['keyword'],
+            'compare' => 'LIKE'
+        )
+    );
+} elseif ( is_archive() ) {
+    $search_args['meta_query'] = array(
+        array(
+            'key'       => 'profession',
+            'value'     => $term,
+            'compare'   => '='
+        )
+    );
+}
+
+
+$count_args = array_merge( $count_args, $search_args );
+
+
+
+$user_count_query = new WP_User_Query($count_args);
+$user_count = $user_count_query->get_results();
+
+// count the number of users found in the query
+$total_users = $user_count ? count($user_count) : 1;
+
+// grab the current page number and set to 1 if no page number is set
+if  (  get_query_var( 'page' ) ) {
+    $page = get_query_var( 'page' );
+} elseif ( get_query_var( 'paged' ) ) {
+    $page = get_query_var( 'paged' );
+} else {
+    $page = 1;
+}
+//$page = get_query_var( 'page' ) ? get_query_var( 'page' ) : 1;
+
+// how many users to show per page
+$users_per_page = 12;
+
+// calculate the total number of pages.
+$total_pages = 1;
+$offset = $users_per_page * ($page - 1);
+$total_pages = ceil($total_users / $users_per_page);
+
+
+/**
+ * WP Query users
+ * @var  $vendors */
+$args  = array(
+    // search only for Authors role
+    'role'      => 'Vendor',
+    // order results by display_name
+    'orderby'   => 'display_name',
+    // return all fields
+    'fields'    => 'all_with_meta',
+
+    'number'    => $users_per_page,
+    'offset'    => $offset // skip the number of users that we have per page
+
+);
+
+$args = array_merge( $args, $search_args );
+
+
+$wp_user_query = new WP_User_Query( $args );
+
+// Get the results
+$vendors = $wp_user_query->get_results();
+
+if ( ! empty( $vendors ) ) : ?>
+    <div class="post post-block">
+        <div class="row">
+            <?php foreach( $vendors as $vendor ):
+                $vendor_info = get_userdata($vendor->ID);
+
+                $profession = '';
+                $categories = wp_get_object_terms( $vendor->ID, 'profession', false );
+
+                $image_id = get_user_meta( $vendor->ID, 'profile_image', true );
+
+                if ( !empty( $categories ) ) $profession = $categories[0]->name;
+
+
+                ?>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="post-item well-block" style="border-bottom: 3px solid <?php echo $vendor_info->color; ?>">
+                        <div class="well-header"><?php echo $profession; ?></div>
+                        <div class="post-img">
+                            <a href="/vendor/<?php echo $vendor->user_login; ?>">
+                                <?php echo wp_get_attachment_image( $image_id, 'vendor-small-thumb' ); ?>
+                            </a>
+                        </div>
+                        <div class="post-content t-sm">
+                            <a href="/vendor/<?php echo $vendor->user_login; ?>" class="post-name">
+                                <?php echo $vendor_info->first_name .' '. $vendor_info->last_name; ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
+
+    <?php
+    $current_url = is_archive() ? get_term_link( $term, 'profession' ) : $current_url = get_permalink( get_page_by_path( 'vendors' ) );
+
+    //ATU_Helper::pagination( $current_url, $total_pages, $page );
+    ?>
+<?php else: ?>
+
+    <h2><?php _e( 'No vendors found', 'atu'); ?></h2>
+
+<?php endif; ?>
--- wp-content/themes/atu/taxonomy-profession.php
+++ wp-content/themes/atu/taxonomy-profession.php
@@ -12,141 +12,21 @@ get_header(); ?>
                         <?php do_action( 'atu_vendor_search_form' ); ?>
                     </div>
                     <div class="page-content">
-                        <div class="post post-block">
-                            <div class="row">
-
-                                <?php
-                                /**
-                                 * We start by doing a query to retrieve all users
-                                 * We need a total user count so that we can calculate how many pages there are
-                                 */
-
-                                $count_args  = array(
-                                    'role'      => 'Vendor',
-                                    'fields'    => 'all_with_meta',
-                                    'meta_query' => array(
-                                        'relation'  => 'OR',
-                                        array(
-                                            'key'       => 'profession',
-                                            'value'     => get_query_var( 'term' ),
-                                            'compare'   => '='
-                                        )
-                                    ),
-                                    'number'    => 999999
-                                );
-
-
-
-
-                                $user_count_query = new WP_User_Query($count_args);
-                                $user_count = $user_count_query->get_results();
-
-                                // count the number of users found in the query
-                                $total_users = $user_count ? count($user_count) : 1;
-
-                                // grab the current page number and set to 1 if no page number is set
-                                $page = get_query_var( 'page' ) ? get_query_var( 'page' ) : 1;
-
-                                // how many users to show per page
-                                $users_per_page = 1;
-
-                                // calculate the total number of pages.
-                                $total_pages = 1;
-                                $offset = $users_per_page * ($page - 1);
-                                $total_pages = ceil($total_users / $users_per_page);
-
-                                $term = get_query_var( 'term' );
-                                /**
-                                 * WP Query users
-                                 * @var  $vendors */
-                                $args  = array(
-                                    // search only for Authors role
-                                    'role'      => 'Vendor',
-                                    // order results by display_name
-                                    'orderby'   => 'display_name',
-                                    // return all fields
-                                    'fields'    => 'all_with_meta',
-                                    'meta_query' => array(
-                                        'relation'  => 'OR',
-                                        array(
-                                            'key'       => 'profession',
-                                            'value'     => $term,
-                                            'compare'   => '='
-                                        )
-                                    ),
-                                    'number'    => $users_per_page,
-                                    'offset'    => $offset // skip the number of users that we have per page
-
-                                );
-
-                                $wp_user_query = new WP_User_Query( $args );
-
-                                // Get the results
-                                $vendors = $wp_user_query->get_results();
-
-                                if ( ! empty( $vendors ) ) :
-                                    foreach( $vendors as $vendor ):
-                                        $vendor_info = get_userdata($vendor->ID);
-
-                                        $profession = '';
-                                        $categories = wp_get_object_terms( $vendor->ID, 'profession', false );
-
-                                        $image_id = get_user_meta( $vendor->ID, 'profile_image', true );
-
-                                        if ( !empty( $categories ) ) $profession = $categories[0]->name;
-
-
-                                        ?>
-
-                                        <div class="col-md-4 col-sm-6">
-                                            <div class="post-item well-block" style="border-bottom: 3px solid <?php echo $vendor_info->color; ?>">
-                                                <div class="well-header"><?php echo $profession; ?></div>
-                                                <div class="post-img">
-                                                    <a href="/vendor/<?php echo $vendor->user_login; ?>">
-                                                        <?php echo wp_get_attachment_image( $image_id, 'vendor-small-thumb' ); ?>
-                                                    </a>
-                                                </div>
-                                                <div class="post-content t-sm">
-                                                    <a href="/vendor/<?php echo $vendor->user_login; ?>" class="post-name">
-                                                        <?php echo $vendor_info->first_name .' '. $vendor_info->last_name; ?>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php endforeach;
-                                else: ?>
-
-                                    <h2><?php _e( 'No vendors found', 'atu'); ?></h2>
-
-                                <?php endif; ?>
-                            </div>
-                        </div>
 
                         <?php
-                        $current_url = get_term_link( $term, 'profession' );
-                        $next = $page < $total_pages ? $page + 1 : $page;
 
-                        echo '<div class="pagination">';
-                        echo '<label for="">Pagination :</label>';
-                        echo '<div class="wp-pagenavi">';
-                        echo '<span class="pages">Page '. $page .' of '. $total_pages .'</span>';
+                            /*
+                             * Include the post format-specific template for the content. If you want to
+                             * use this in a child theme, then include a file called called content-___.php
+                             * (where ___ is the post format) and that will be used instead.
+                             */
+                            get_template_part( 'content', 'vendors' );
 
-                        for( $i = 1; $i <= $total_pages; $i++ ) {
-                            if ( $i == $page ) {
-                                echo '<span class="current">'. $page .'</span>';
-
-                            } else {
-
-                                echo '<a class="page larger" href="'. $current_url . '?page=' . $i .'/">'. $i .'</a>';
-                            }
-                        }
-
-
-                        echo '<a class="nextpostslink" rel="next" href="'. $current_url . '?page=' . $next .'/">Next</a>';
-                        echo '</div>';
-                        echo '</div>';
 
                         ?>
+
+
+
                     </div>
                 </div>
             </div>
