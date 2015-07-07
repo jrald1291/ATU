0c012e184190d1fce3ea88a1cc1be21a67ea7717
Sergio <serg.casquejo@yahoo.com>
2015-07-08 06:31:10 +0800

    update

--- atu.sql
+++ atu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2015 at 12:04 AM
+-- Generation Time: Jul 08, 2015 at 12:29 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
