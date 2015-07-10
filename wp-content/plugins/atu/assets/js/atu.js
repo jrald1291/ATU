92e3ba858280978e83ad8456e21852a1fd832196
jrald1291 <jrald1291@gmail.com>
2015-07-11 01:16:24 +0800

    update

--- wp-content/plugins/atu/assets/js/atu.js
+++ wp-content/plugins/atu/assets/js/atu.js
@@ -15,12 +15,28 @@
     };
 
     D.ready(function() {
-
-
-        $('body').on('change', 'select#filterType', function() {
-            var val = $(this).val();
+            var val = $('select#filterType').val();
             var $postcode = $('select[name=post_code]');
             var $region = $('select[name=region]');
+        if ( val == 'post_code' ) {
+                $postcode.removeClass('hidden').removeAttr('disabled');
+                $postcode.parent('.form-group').parent('.col-md-2').removeClass('hidden');
+                $region.attr('disabled', true);
+                $region.parent('.form-group').parent('.col-md-2').addClass('hidden');
+            } else if(val == 'region') {
+                $region.removeClass('hidden').removeAttr('disabled');
+                $region.parent('.form-group').parent('.col-md-2').removeClass('hidden');
+                $postcode.attr('disabled', true);
+                $postcode.parent('.form-group').parent('.col-md-2').addClass('hidden');
+            } else {
+                $region.addClass('hidden').attr('disabled', true);
+                $postcode.addClass('hidden').attr('disabled', true);
+            }
+
+        $('body').on('change', 'select#filterType', function() {
+             val = $(this).val();
+             $postcode = $('select[name=post_code]');
+             $region = $('select[name=region]');
             if ( val == 'post_code' ) {
                 $postcode.removeClass('hidden').removeAttr('disabled');
                 $postcode.parent('.form-group').parent('.col-md-2').removeClass('hidden');
