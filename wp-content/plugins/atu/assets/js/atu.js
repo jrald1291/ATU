22f851fc84b5d7e994263e1de97e564eebcfaaaa
sergiocasquejo <serg.casquejo@yahoo.com>
2015-07-10 21:20:53 +0800

    update

--- wp-content/plugins/atu/assets/js/atu.js
+++ wp-content/plugins/atu/assets/js/atu.js
@@ -15,28 +15,28 @@
     };
 
     D.ready(function() {
-        $('.atu-membership-form').on('submit', function(e) {
-            e.preventDefault();
-            var $form = $(this);
-            var params = $(this).serialize() + '&action=post-membership'
-            Data.post(ATU.ajaxurl, params).done(function(results) {
-                results = $.parseJSON(results);
-                $('.atu-post-message').remove();
-
-                if (results.message instanceof Array) {
-                    for ( var i in results.message ) {
-                        $form.append('<p class="atu-post-message message-' + results.status + '">' + results.message[i] + '</p>');
-                    }
-                } else{
-                    $form.append('<p class="atu-post-message message-' + results.status + '">' + results.message + '</p>');
-                }
-
-                if (results.status == 'success') $form.find('input[type=email]').val('');
-            });
-
-
-            return false;
-        });
+        //$('.atu-membership-form').on('submit', function(e) {
+        //    e.preventDefault();
+        //    var $form = $(this);
+        //    var params = $(this).serialize() + '&action=post-membership'
+        //    Data.post(ATU.ajaxurl, params).done(function(results) {
+        //        results = $.parseJSON(results);
+        //        $('.atu-post-message').remove();
+        //
+        //        if (results.message instanceof Array) {
+        //            for ( var i in results.message ) {
+        //                $form.append('<p class="atu-post-message message-' + results.status + '">' + results.message[i] + '</p>');
+        //            }
+        //        } else{
+        //            $form.append('<p class="atu-post-message message-' + results.status + '">' + results.message + '</p>');
+        //        }
+        //
+        //        if (results.status == 'success') $form.find('input[type=email]').val('');
+        //    });
+        //
+        //
+        //    return false;
+        //});
 
 
         $('select#filterType').on('change', function(e) {
