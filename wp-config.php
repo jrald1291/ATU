fa43641dc309cdc63d2d8a58ed76adf903b44fe5
jrald1291 <jrald1291@gmail.com>
2015-07-11 14:43:27 +0800

    update

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_NAME', 'atu');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
--- wp-content/themes/atu/page-templates/contact.php
+++ wp-content/themes/atu/page-templates/contact.php
@@ -23,9 +23,13 @@ get_header(); ?>
 	}
 
 	$user_id =1;
+	$post_id =''; 
 
 	if(isset($_GET["contact_id"])){
 		$user_id = $_GET["contact_id"];
+	}
+	if(isset($_GET["post_id"])){
+		$post_id = $_GET["post_id"];
 	}
 
 	$user_info =  get_userdata($user_id);
@@ -52,16 +56,36 @@ get_header(); ?>
 						</div>
 						<div class="section section-contact">
 							<div class="contact-avatar">
-								<?php echo wp_get_attachment_image( $user_info->profile_image, 'thumbnail' ); ?>
+								<?php if ($post_id) {?>
+									<?php echo wp_get_attachment_image( get_post_thumbnail_id( $post_id ),'thumbnail'); ?>
+								<?php }else{ ?>
+									<?php echo wp_get_attachment_image( $user_info->profile_image, 'thumbnail' ); ?>
+								<?php } ?>
 							</div>
-							<div class="contact-name"> Hi! I am <?php echo $user_info->first_name ?> <?php echo $user_info->last_name ?></div>
-							<div class="contact-company"><?php echo $user_info->company_name ?></div>
-							<div class="contact-content">Call me if you prefer to speak to a real life person</div>
-							<ul class="list list-contact list-inline">
-								<li><a href="tel:<?php echo $user_info->mobile; ?>"><span class="fa fa-mobile icon-l-sm"></span><?php echo $user_info->mobile; ?></a></li>
-	                            <li><a href="tel:<?php echo $user_info->phone; ?>"><span class="fa fa-phone icon-l-sm"></span><?php echo $user_info->phone; ?></a></li>
-	                            <li><a href="mailto:<?php echo $user_info->user_email; ?>"><span class="fa fa-envelope icon-l-sm"></span><?php echo $user_info->user_email; ?></a></li>
-							</ul>
+							<?php if ($post_id) {?>
+
+								<div class="contact-name"> Welcome to <?php echo get_field('company_name',$post_id) ?></div>
+								<div class="contact-company">Address: <?php echo get_field('address',$post_id) ?> <?php echo get_field('region',$post_id) ?> <?php echo get_field('post_code',$post_id) ?></div>
+								<div class="contact-content">Call us if you prefer to speak to a real life person</div>
+								<ul class="list list-contact list-inline">
+									<li><a href="tel:<?php echo get_field('mobile',$post_id) ?>"><span class="fa fa-mobile icon-l-sm"></span><?php echo get_field('mobile',$post_id) ?></a></li>
+		                            <li><a href="tel:<?php echo get_field('phone',$post_id) ?>"><span class="fa fa-phone icon-l-sm"></span><?php echo get_field('phone',$post_id) ?></a></li>
+		                            <li><a href="mailto:<?php echo get_field('email',$post_id) ?>"><span class="fa fa-envelope icon-l-sm"></span><?php echo get_field('email',$post_id) ?></a></li>
+								</ul>
+
+							<?php }else{ ?>
+
+								<div class="contact-name"> Hi! I am <?php echo $user_info->first_name ?> <?php echo $user_info->last_name ?></div>
+								<div class="contact-company"><?php echo $user_info->company_name ?></div>
+								<div class="contact-content">Call me if you prefer to speak to a real life person</div>
+								<ul class="list list-contact list-inline">
+									<li><a href="tel:<?php echo $user_info->mobile; ?>"><span class="fa fa-mobile icon-l-sm"></span><?php echo $user_info->mobile; ?></a></li>
+		                            <li><a href="tel:<?php echo $user_info->phone; ?>"><span class="fa fa-phone icon-l-sm"></span><?php echo $user_info->phone; ?></a></li>
+		                            <li><a href="mailto:<?php echo $user_info->user_email; ?>"><span class="fa fa-envelope icon-l-sm"></span><?php echo $user_info->user_email; ?></a></li>
+								</ul>
+
+							<?php } ?>
+							
 						</div>				
 						<?php echo do_shortcode('[contact-form-7 id="196" title="Contact ATU" html_class="form form-labeled"]'); ?>
 					</div>
@@ -74,8 +98,14 @@ get_header(); ?>
 	</div>
 </div>
 <script type="text/javascript">
-  document.getElementById("user_email").value = "<?php echo $user_info->user_email; ?>";
-  document.getElementById("company_name").value = "<?php echo $user_info->company_name ?>";
+	<?php if ($post_id) {?>
+		document.getElementById("user_email").value = "<?php echo get_field('email',$post_id) ?>";
+  		document.getElementById("company_name").value = "<?php echo get_field('company_name',$post_id) ?>";
+	<?php }else{ ?>
+		document.getElementById("user_email").value = "<?php echo $user_info->user_email; ?>";
+  		document.getElementById("company_name").value = "<?php echo $user_info->company_name ?>";
+	<?php } ?>
+  
 </script>
 <?php get_footer(); ?>
 
