5e68c33e2906ddcb4e0633a72e1982eab1504776
jrald1291 <jrald1291@gmail.com>
2015-07-27 20:05:44 +0800

    update

--- wp-content/themes/wepn/taxonomy-venue-category.php
+++ wp-content/themes/wepn/taxonomy-venue-category.php
@@ -82,6 +82,11 @@ get_header();
             </div>
             <div class="col-md-3">
                 <aside class="l-sidebar">
+                    <?php if (!is_user_logged_in() ) { ?> 
+                      <div class="widget">
+                          <a href="<?php echo wp_login_url();?>" class="btn btn-primary btn-block mb-20">Member Login</a>
+                      </div>
+                    <?php } ?>
                     <?php if (of_get_option('video_venue', '')) {?>
                         <div class="widget">
                             <a href="#main_vid" role="button" data-toggle="modal" class="btn btn-primary btn-block mb-20"><span class="fa fa-play-circle icon-l-sm"></span>WEPN Venue Video</a>
